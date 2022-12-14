3a9e6563c7f1060c2634f7fc3657d5f0358af2f9
rybaier <rybaier@gmail.com>
2022-12-13 21:27:43 -0500

    exercise complete working on bonus

--- variables/target/debug/deps/variables-638964cb1051eb61.d
+++
@@ -1,5 +0,0 @@
-/Users/ryanbaier/Desktop/seir-1213-and-udemy/Rust-Udemy/Rust-crash-course/variables/target/debug/deps/variables-638964cb1051eb61.rmeta: src/main.rs
-
-/Users/ryanbaier/Desktop/seir-1213-and-udemy/Rust-Udemy/Rust-crash-course/variables/target/debug/deps/variables-638964cb1051eb61.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/deps/variables-a134881eedc512b4.d
+++
@@ -1,5 +0,0 @@
-/Users/ryanbaier/Desktop/seir-1213-and-udemy/Rust-Udemy/Rust-crash-course/variables/target/debug/deps/variables-a134881eedc512b4.rmeta: src/main.rs
-
-/Users/ryanbaier/Desktop/seir-1213-and-udemy/Rust-Udemy/Rust-crash-course/variables/target/debug/deps/variables-a134881eedc512b4.d: src/main.rs
-
-src/main.rs:
