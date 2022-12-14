1b4e91ba2bce8dbe0df27b9094d58b68d7f2b7ba
rybaier <rybaier@gmail.com>
2022-12-13 21:45:44 -0500

    bonus complete

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,23 @@
 fn main() {
+    const STARTING_MISSILES: i32 = 8;
+    const READY_AMOUNT: i32 = 2;
+    const MISSION_COMPLETE: &str = "Mission Complete, let's go home"; 
     let mut missiles: i32 = STARTING_MISSILES;
     let ready: i32 = READY_AMOUNT;
-
     println!("Firing {} of my {} missiles...", ready, missiles);
     missiles = missiles - ready;
     println!("{} missiles left...", missiles);
 
-    const STARTING_MISSILES: i32 = 8;
-    const READY_AMOUNT: i32 = 2;
+    let (remaining_missiles, mut target)= (missiles, "target missed");
+
+    println!("{} you have {} more shots", target, remaining_missiles);
+    
+    target="target acquired";
+    println!("{}",target);
+    println!("Firing all {} missiles!", remaining_missiles);
+    target = "DIRECT HIT!!!";
+    println!("{}",target);
+    println!("{}",MISSION_COMPLETE);
+    
 
 }
