998732d030e0da84575efbabf2815d540b9fca2c
rybaier <rybaier@gmail.com>
2022-12-17 16:34:03 -0500

    collections and enums exercise match is much more useful than elif

--- collections_enums/src/main.rs
+++ collections_enums/src/main.rs
@@ -7,17 +7,28 @@
 // - `Bullseye`
 // - `Hit`, containing the distance from the center (an f64)
 // - `Miss`
-//
+ enum Shot {
+    Bullseye,
+    Hit(f64),
+    Miss
+ }
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
+        
         // 1b. Implement this method to convert a Shot into points
         // - return 5 points if `self` is a `Shot::Bullseye`
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
         // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
         // - return 0 points if `self` is a Miss
+        match self {
+            Shot::Bullseye => 5,
+            Shot::Hit(x) if x < 3.0 => 2, 
+            Shot::Hit(x) => 1,
+            Shot::Miss => 0
+            }
     }
 }
 
@@ -34,10 +45,30 @@ fn main() {
     //      - Less than 1.0 -- `Shot::Bullseye`
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
-
+    for arrow in arrow_coords {
+        arrow.print_description();
+        // I used elif statment which got the job done
+        // if arrow.distance_from_center() < 1.0 {
+        //     shots.push(Shot::Bullseye)
+        // } else if arrow.distance_from_center() > 1.0 && arrow.distance_from_center() < 5.0 {
+        //     shots.push(Shot::Hit(i.distance_from_center()))
+        // } else{
+        //     shots.push(Shot::Miss)
+        // };
+        // his version was cleaner using a match expression
+        let shot = match arrow.distance_from_center() {
+            x if x < 1.0 =>  Shot::Bullseye,
+            x if x < 5.0 => Shot::Hit(x),
+            _ => Shot::Miss
+        };
+        shots.push(shot)
+    }
 
     let mut total = 0;
     // 3. Finally, loop through each shot in shots and add its points to total
+   for shot in shots {
+        total += shot.points()
+   }
 
     println!("Final point total is: {}", total);
 }
